53acb5716d32d10603d6f55d414418e415e29a9b
MonirSaikat <moniruzzamansaikat@yahoo.com>
2023-01-02 18:13:22 +0600

    responsive table

--- src/components/Table.js
+++ src/components/Table.js
@@ -25,12 +25,14 @@ const Table = ({ data, config }) => {
   });
 
   return (
-    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-      <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-        <tr className="border-1">{renderHeaders}</tr>
-      </thead>
-      <tbody>{renderRows}</tbody>
-    </table>
+    <div className='overflow-x-auto'>
+      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr className="border-1">{renderHeaders}</tr>
+        </thead>
+        <tbody>{renderRows}</tbody>
+      </table>
+    </div>
   );
 }
 
