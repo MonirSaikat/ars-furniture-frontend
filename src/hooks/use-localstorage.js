dc6ef24324bed8353b1fb1042972b732fbf672ad
MonirSaikat <moniruzzamansaikat@yahoo.com>
2023-01-02 12:59:48 +0600

    import usestate

--- src/hooks/use-localstorage.js
+++ src/hooks/use-localstorage.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 export function useLocalStorage(key, initialValue) {
   const [storedValue, setStoredValue] = useState(() => {
     if (typeof window === "undefined") {
