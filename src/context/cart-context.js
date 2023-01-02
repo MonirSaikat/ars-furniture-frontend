0f98a1ec6900218652e34e2ce88717b622a1f348
MonirSaikat <moniruzzamansaikat@yahoo.com>
2023-01-02 14:19:42 +0600

    handle duplicate item in cart

--- src/context/cart-context.js
+++ src/context/cart-context.js
@@ -7,17 +7,28 @@ export const CartProvider = ({ children }) => {
   const [cartState, setCartState] = useState([]);
   const [cart, setCart] = useLocalStorage('cart');
 
-  useEffect(() => setCartState(cart), []);
+  useEffect(() => setCartState(cart ? cart : []), []);
 
   const addToCart = (product) => {
-    const updatedCart = cart ? [...cart, product] : [product];
-    setCart(updatedCart);
+    const alreadyInCart = cartState.find(p => p._id === product._id);
+    if(alreadyInCart) {
+      const updated = cartState.map(c => {
+        if (c._id === product._id) {
+          c.quantity += product.quantity;
+        }
+        return c;
+      });
+      setCartState(updated);
+    } else {
+      setCartState([...cartState, product]);
+    }
+    setCart(cartState);
   };
 
   const removeFromCart = () => {};
 
   const cartLength = () => {
-    return !cartState ? 0 : cartState.length;
+    return cartState.length;
   };
 
   const data = {
