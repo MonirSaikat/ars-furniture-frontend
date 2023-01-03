b91d5a1f79c59f8b75e40f7a4dd18aae3c74c162
MonirSaikat <moniruzzamansaikat@yahoo.com>
2023-01-03 07:57:57 +0600

    added checkout page

---
+++ src/pages/CheckoutPage.js
@@ -0,0 +1,9 @@
+const CheckoutPage = ({  }) => {
+  return(
+    <div className="container mx-auto">
+      <h1 className="text-center">Checkout page</h1>
+    </div>
+  );
+};
+
+export default CheckoutPage;
