00f37d3ba14f6095d0cac1fe212ed621d00cb1d6
MonirSaikat <moniruzzamansaikat@yahoo.com>
2023-01-03 08:18:17 +0600

    separate admin nav

--- src/components/layouts/DashboardLayout.js
+++ src/components/layouts/DashboardLayout.js
@@ -1,29 +1,11 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Sidebar from '../admin/Sidebar';
-import Button from '../Button';
-import { AiOutlineLogout } from "react-icons/ai";
-import { useAuth } from '../../hooks/use-auth';
+import AdminNav from '../admin/AdminNav';
 
 const DashboardLayout = () => {
-  const { user, logout } = useAuth();
-
-  const renderTopBar = (
-    <div className="h-16 bg-gray-900 flex justify-between pl-4 items-center pr-5 fixed w-full">
-      <Link to="/dashboard">
-        <strong className="text-gray-300">
-          {user.name}'s Dashboard
-        </strong>
-      </Link>
-      <Button danger onClick={logout}>
-        <AiOutlineLogout className="mr-3" />
-        Logout
-      </Button>
-    </div>
-  );
-
   return (
     <div>
-      {renderTopBar}
+      <AdminNav />
 
       <div className="">
         <Sidebar />
