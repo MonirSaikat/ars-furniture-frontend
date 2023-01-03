a200feda7a72b733e6e7869dd2cee6cd78ecd949
MonirSaikat <moniruzzamansaikat@yahoo.com>
2023-01-03 08:19:39 +0600

    separate cart subtotal

---
+++ src/components/CartSubtotal.js
@@ -0,0 +1,15 @@
+import { useCart } from '../hooks/use-cart';
+import Button from './Button';
+
+const CartSubtotal = () => {
+  const { cartState, subtotalPrice } = useCart();
+
+  return (
+    <div className='shadow-lg p-3 self-start'>
+      <p className='text-xl mb-2'>Subtotal ({cartState.length} items): $<strong>{subtotalPrice}</strong></p>
+      <Button primary link rounded className='w-full font-light' to='/checkout'>Proceed to buy</Button>
+    </div>
+  );
+};
+
+export default CartSubtotal;
