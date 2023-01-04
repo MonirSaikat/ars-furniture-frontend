1b1b89a26bd0b259b74ccf226fd8374a1f7ecbbe
MonirSaikat <moniruzzamansaikat@yahoo.com>
2023-01-04 13:58:13 +0600

    responsive header

--- src/components/admin/AdminNav.js
+++ src/components/admin/AdminNav.js
@@ -10,10 +10,10 @@ const AdminNav = () => {
     <div className="h-16 bg-gray-900 flex justify-between pl-4 items-center pr-5 fixed w-full">
       <Link to="/dashboard">
         <strong className="text-gray-300">
-          {user.name}'s Dashboard
+          Dashboard
         </strong>
       </Link>
-      <Button danger onClick={logout}>
+      <Button danger onClick={logout} className='text-xs px-2 py-1 bg-red-600 rounded-sm'>
         <AiOutlineLogout className="mr-3" />
         Logout
       </Button>
