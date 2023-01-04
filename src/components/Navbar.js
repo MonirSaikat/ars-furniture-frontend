a36444927bc47c0ad51317e9973cfacdda877580
MonirSaikat <moniruzzamansaikat@yahoo.com>
2023-01-04 13:45:58 +0600

    mobile nva

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { createRef, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../hooks/use-auth';
 import { useCart } from '../hooks/use-cart';
 import Button from './Button';
+import { AiOutlineMenu } from 'react-icons/ai';
 
 const Navbar = () => {
   const { loggedIn } = useAuth();
   const { cartLength } = useCart();
+  const [collapsed, setCollapsed] = useState(false);
+  const menuRef = createRef();
+
+  useEffect(() => {
+    const handler = (e) => {
+      if(!menuRef?.current?.contains(e.target)) {
+        setCollapsed(false);
+      };
+    };
+
+    document.addEventListener('click', handler, true);
+
+    return () => document.removeEventListener('click', handler);
+  }, []);
 
   const links = [{
     path: '/',
@@ -27,7 +42,7 @@ const Navbar = () => {
 
   const renderLinks = links.map((link) => {
     return (
-      <li key={link.path} className='mx-3'>
+      <li key={link.path} className='mx-3 my-2 md:my-0'>
         <Link to={link.path} href="#">
           {link.label}
         </Link>
@@ -53,7 +68,17 @@ const Navbar = () => {
           /> */}
         </Link>
 
-        <ul className="flex items-center">
+        <button
+          aria-controls='Navbar toggler'
+          className='text-2xl hover:text-gray-200'
+          onClick={() => setCollapsed(!collapsed)}
+        >
+          <AiOutlineMenu />
+        </button>
+
+        <ul
+          ref={menuRef}
+          className={`flex items-center flex-col md:flex-col absolute bg-gray-900 z-20 w-full left-0 top-[70px] ${collapsed ? 'opacity-100 h-auto' : 'opacity-0'} py-5 duration-500`}>
           {renderLinks}
           <li className="ml-4">
             <Button link to={ loggedIn ? "/dashboard" : "/login"} primary>
