de3dffffa598cb50999d09132d240dfa0d4080a2
MonirSaikat <moniruzzamansaikat@yahoo.com>
2023-01-04 13:18:43 +0600

    added token for products services

--- src/context/product-context.js
+++ src/context/product-context.js
@@ -1,17 +1,28 @@
-import { createContext, useState, useEffect } from 'react';
-import { addNewProduct, deleteProductById, getAllProducts } from '../services/product-service';
+import {
+  createContext,
+  useState,
+  useEffect
+} from 'react';
+import {
+  addNewProduct,
+  deleteProductById,
+  getAllProducts
+} from '../services/product-service';
+import { useAuth } from '../hooks/use-auth';
 
 export const ProductContext = createContext();
 
 const ProductsProvider = ({ children }) => {
+  const { token } = useAuth();
   const [products, setProducts] = useState([]);
   const [fetching, setFetching] = useState(false);
 
   const fetchProducts = () => {
-    getAllProducts().then((prodArr) => {
-      setProducts(prodArr);
-      setFetching(false);
-    });
+    getAllProducts()
+      .then((prodArr) => {
+        setProducts(prodArr);
+        setFetching(false);
+      });
   };
 
   useEffect(() => {
@@ -20,14 +31,14 @@ const ProductsProvider = ({ children }) => {
   }, []);
 
   const addProduct = (product) => {
-    addNewProduct(product)
+    addNewProduct(product, token)
       .then(product => {
         setProducts(prevProducts => [...prevProducts, product]);
       });
   };
 
   const deleteProduct = (id) => {
-    deleteProductById(id)
+    deleteProductById(id, token)
       .then(() => {
         const updatedProducts = products.filter(product => product._id !== id);
         setProducts(updatedProducts);
--- src/services/product-service.js
+++ src/services/product-service.js
@@ -1,9 +1,11 @@
 import { handleError, handleSuccess } from '../utils';
 import { api } from './api';
 
-export const addNewProduct = async (data) => {
+export const addNewProduct = async (data, token) => {
   try {
-    const product = await api.post('/products', {...data}, true);
+    const product = await api.
+      post('/products', {...data}, token);
+
     handleSuccess('Product Added');
     return product;
   } catch (error) {
@@ -12,7 +14,7 @@ export const addNewProduct = async (data) => {
 };
 
 export const getAllProducts = async () => {
-  const products = await api.get('products');
+  const products = await api.get('products', token);
   return products;
 };
 
@@ -20,9 +22,9 @@ export const getProductById = async (id) => {
   return await api.get(`products/${id}`);
 };
 
-export const deleteProductById = async (id) => {
+export const deleteProductById = async (id, token) => {
   try {
-    await api.delete(`products/${id}`);
+    await api.delete(`products/${id}`, token);
     handleSuccess('Product deleted');
   } catch (error) {
     handleError(error);
