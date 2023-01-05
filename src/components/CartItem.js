9eee6c8da456f1f892c070aab4a6feb7fd84bed3
MonirSaikat <moniruzzamansaikat@yahoo.com>
2023-01-05 12:24:36 +0600

    responsive shopping cart

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -23,28 +23,33 @@ const CartItem = ({ cart }) => {
   };
 
   return (
-    <div className="p-3 flex justify-between mb-3 border-b">
-      <div className="flex">
+    <div className="p-3 flex flex-col md:flex-row justify-between mb-3 border-b">
+      <div className="flex flex-col md:flex-row">
         <img src={imageUrl} alt={label} className="h-28 w-40" />
-        <div className="ml-2">
+        <div className="ml-0 md:ml-2">
           <h2 className="text-2xl mb-2">{label}</h2>
-          <div className="flex">
+          <div className="flex flex-col md:flex-row my-2 md:my-0">
             <input
               type="number"
-              className="border-2 p-1"
+              className="border-2 p-1 mb-2 md:mb-0"
               value={quantity}
               onChange={handleQuantityChange}
             />
-            <Button sm primary onClick={handleUpdateCart}>
-              Update
-            </Button>
-            <Button sm danger onClick={() => handleRemove(cart)}>
-              Remove
-            </Button>
+            <div className='flex'>
+              <Button sm primary onClick={handleUpdateCart}>
+                Update
+              </Button>
+              <Button sm danger onClick={() => handleRemove(cart)}>
+                Remove
+              </Button>
+            </div>
           </div>
         </div>
       </div>
-      <div>${cart.price}</div>
+      <div>
+        <span className="md:hidden">Price : </span>
+        ${cart.price}
+      </div>
     </div>
   );
 };
--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useCart } from "../hooks/use-cart";
 import CartSubtotal from "../components/CartSubtotal";
 import CartItem from "../components/CartItem";
@@ -26,11 +26,11 @@ const CartPage = () => {
 
   return (
     <div className="container mx-auto py-8">
-      <div className="grid grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-y-4 md:gap-4">
         <div className="shadow-lg p-3 col-span-3">
           <header className="flex justify-between mb-3 border-b pb-3">
             <p className="text-3xl">Shopping Cart</p>
-            <p className="text-2xl font-light">Price</p>
+            <p className="text-2xl font-light hidden md:block">Price</p>
           </header>
           {cartState.length > 0 ? renderCartItems : noItemsInCart}
           {renderFooter}
