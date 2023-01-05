07d937472b5897dd3b0dbb353dbe5a2d9326f94b
MonirSaikat <moniruzzamansaikat@yahoo.com>
2023-01-05 12:25:14 +0600

    responsive font size

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     <header className="bg-header md:h-96 py-14 sm:h-full bg-center bg-cover bg-blend-overlay">
       <div className="container mx-auto text-white flex items-center h-full px-5 md:px-0">
         <div className="max-w-4xl mx-auto text-center">
-          <h1 className="text-6xl font-semibold">Order your dream decor now</h1>
+          <h1 className="text-4xl md:text-6xl font-semibold">Order your dream decor now</h1>
           <p className="my-4 text-sm text-gray-100 font-light">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
             eveniet nesciunt labore laborum porro fuga. Ab iste aperiam odit
